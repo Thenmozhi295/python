7b4e8c20cedb402d1187be12a215466d01486bb3
Thenmozhi295 <85125273+Thenmozhi295@users.noreply.github.com>
2021-06-02 14:35:19 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,33 @@
+
+print("Hello World")
+
+a=10
+b="Hi Python"
+c=10.5
+
+print(a)
+print(b)
+print(c)
+
+a=input("Enter here:")
+print("The entered number is",a)
+
+a=1
+print(type(a))
+
+
+b=1.33
+print(type(b))
+
+c={1,2,3,4}
+print(type(c))
+
+d=set([1,2,3,4])
+print(type(d))
+
+e={1:'one','two' :2,2.3 :'what','john' :12}
+print(type(e))
+print(e)
+
+f=(1,2,3,4)
+print(type(f))
